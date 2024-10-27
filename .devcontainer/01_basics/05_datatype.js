49fde0f5437266f5cea1c23dba178b2c9b10520c
Ritesh Kushwaha <riteshkushwaha497@gmail.com>
2024-10-27 12:21:05 +0000

    summary

---
+++ .devcontainer/01_basics/05_datatype.js
@@ -0,0 +1,22 @@
+// primitive
+
+const outsideTemp = null;
+let userEmail;
+
+const id = Symbol('123');
+const anotherId = Symbol('123');
+console.log(id==anotherId)
+
+// bigINt, string, number, boolean, symbol, null, undefined 7 primitive data types
+
+// Non primitve (reference datatye)
+
+// function objects array
+
+const myFunction = function(){
+    console.log(nothing);
+}
+console.log(typeof myFunction);
+
+// false
+// function

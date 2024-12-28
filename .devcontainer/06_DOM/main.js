6b2a4a3f1291bf0904a6ec539d12585c845bac36
Ritesh Kushwaha <riteshkushwaha497@gmail.com>
2024-12-28 19:36:52 +0530

    Add files via upload

---
+++ .devcontainer/06_DOM/main.js
@@ -0,0 +1,31 @@
+// let h1 = document.getElementsByClassName("bg")
+// h1.innerHTML = "HEllo"
+
+
+// arr.forEach(function(i) {
+//     i.sytle.backgroundColor="black"
+//                       })
+// arr.forEach(function(i) {
+//     i.style.backgroundColor="black"
+//                       })
+// arr.forEach(function(i) {
+//     i.style.backgroundColor="black"
+//     i.style.color='white'
+//                       })
+
+
+
+
+
+// document.getElementById
+// document.getElementsByClassName
+// document.getElementsByTagName
+// document.getElementsByName
+
+
+
+
+
+
+
+
